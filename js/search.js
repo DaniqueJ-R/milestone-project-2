c6f6bbebc97319e919687e1ec5d5f64557c92003
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-04-03 23:13:46 +0100

    Updated test area and added test js to confirm connected correctly

--- js/search.js
+++ js/search.js
@@ -1,10 +1,9 @@
-const songList = [
-    { title: "Shape of You", artist: "Ed Sheeran" },
-    { title: "Blinding Lights", artist: "The Weeknd" },
-    { title: "Levitating", artist: "Dua Lipa" },
-    { title: "Someone Like You", artist: "Adele" },
-    { title: "Rolling in the Deep", artist: "Adele" },
-    { title: "Old Town Road", artist: "Lil Nas X" },
-    { title: "Stay", artist: "Justin Bieber" }
-  ];
+const currentStep = 0;
+
+function nextFunction() {
+      addEventListener('click', () => {
+          console.log(`Current step is now: ${currentStep}`);
+      });
+  }
   
+  module.exports = { currentStep };
--- js/test/search.test.js
+++ js/test/search.test.js
@@ -1,7 +1,16 @@
+const { currentStep } = require("../search.js"); 
+
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
+});
+
+describe("currentStep to show correct number", () => {
+    test("currentStep should be set to 0", () => {
+        console.log(currentStep); 
+        expect(currentStep).toEqual(0); 
+    });
 });
