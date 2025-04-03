6f3cbad47b8c9ef46d763f0493cab93d0f895e65
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-04-03 01:42:34 +0100

    Corrected test and js file, added jest for text section, added notes for general brainstorm progress

---
+++ js/search.js
@@ -0,0 +1,10 @@
+const songList = [
+    { title: "Shape of You", artist: "Ed Sheeran" },
+    { title: "Blinding Lights", artist: "The Weeknd" },
+    { title: "Levitating", artist: "Dua Lipa" },
+    { title: "Someone Like You", artist: "Adele" },
+    { title: "Rolling in the Deep", artist: "Adele" },
+    { title: "Old Town Road", artist: "Lil Nas X" },
+    { title: "Stay", artist: "Justin Bieber" }
+  ];
+  
