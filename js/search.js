33122372e8563c0f589ee89216947a3bba360f8c
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-04-05 03:22:48 +0100

    confirmed nextFunction working to change html. styled css to make info overlap correctly.

--- js/search.js
+++ js/search.js
@@ -1,9 +1,34 @@
-const currentStep = 0;
+const stepTracker = { currentStep: 0 }
+    
+// document.getElementById("nextButton").addEventListener("click", () => {
+    // nextFunction();
+    // });
+
+
 
 function nextFunction() {
-      addEventListener('click', () => {
-          console.log(`Current step is now: ${currentStep}`);
-      });
-  }
-  
-  module.exports = { currentStep };
+        let currentPage = document.getElementById(`step${stepTracker.currentStep}`);
+        if (currentPage) {
+            currentPage.classList.remove("active");
+        }
+
+        stepTracker.currentStep++; 
+
+        let nextPage = document.getElementById(`step${stepTracker.currentStep}`);
+        if (nextPage) {
+            nextPage.classList.add("active");
+        }
+
+        if (stepTracker.currentStep > 2) { // Assuming there are 3 steps (0, 1, 2) is actually 5 steps, update later
+            stepTracker.currentStep = 2; // Prevent going beyond the last step
+        }
+    };
+
+
+function noLogIn() {
+    alert("{Pleasae remeber if you don't log in, you will have limited use!");
+    nextFunction()
+}    
+
+
+module.exports = { stepTracker, nextFunction, noLogIn };
--- js/test/search.test.js
+++ js/test/search.test.js
@@ -1,16 +1,73 @@
-const { currentStep } = require("../search.js"); 
-
-beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-});
+const { stepTracker, nextFunction, noLogIn} = require("../search.js");
 
 describe("currentStep to show correct number", () => {
-    test("currentStep should be set to 0", () => {
-        console.log(currentStep); 
-        expect(currentStep).toEqual(0); 
-    });
+  test("currentStep should be set to 0", () => {
+    expect(stepTracker.currentStep).toEqual(0);
+  });
 });
+
+describe("nextFunction working correctly", () => {
+  beforeEach(() => {
+    // Set up the initial DOM state for each test
+    stepTracker.currentStep = 0; // Reset currentStep before each test
+    document.body.innerHTML = `
+            <div id="step0" class="active">Step 0</div>
+            <div id="step1" class="page">Step 1</div>
+            <div id="step2" class="page">Step 2</div>
+            <button id="nextButton">Next</button>
+        `;
+  });
+
+  afterEach(() => {
+    stepTracker.currentStep = 0;
+    document.body.innerHTML = "";
+  });
+
+  test("nextFunction should find the current step element", () => {
+    let currentPage = document.getElementById(`step${stepTracker.currentStep}`);
+    expect(currentPage).not.toBeNull();
+  });
+
+  test("nextFunction should remove active class from current step", () => {
+    let currentPage = document.getElementById(`step${stepTracker.currentStep}`);
+    expect(currentPage.classList.contains("active")).toBe(true);
+    nextFunction();
+    expect(currentPage.classList.contains("active")).toBe(false);
+  });
+
+  test("nextFunction should increment currentStep", () => {
+    nextFunction();
+    expect(stepTracker.currentStep).toEqual(1);
+  });
+
+  test("nextFunction should add active class to next step", () => {
+    nextFunction();
+    let nextPage = document.getElementById(`step${stepTracker.currentStep}`);
+    expect(nextPage.classList.contains("active")).toBe(true);
+  });
+});
+
+test("nextFunction should not exceed the number of steps", () => {
+  stepTracker.currentStep = 2; // Set to the last step
+  nextFunction(); // Call nextFunction to try to go to the next step
+  expect(stepTracker.currentStep).toEqual(2); // Should not exceed the last step
+});
+
+describe("noLogIn function conntinues sequence correctly", () => {
+    beforeEach(() => {
+        stepTracker.currentStep = 1; // Reset currentStep before each test
+        document.body.innerHTML = `
+                <div id="step0" class="active">Step 0</div>
+                <div id="step1" class="page">Step 1</div>
+                <div id="step2" class="page">Step 2</div>
+                <button id="nextButton">Next</button>
+            `;
+    });
+    
+    
+    test("noLogIn should alert the user", () => {
+        global.alert = jest.fn(); // Mock the alert function
+        noLogIn();
+        expect(global.alert).toHaveBeenCalledWith("{Pleasae remeber if you don't log in, you will have limited use!");
+    });
+    });
